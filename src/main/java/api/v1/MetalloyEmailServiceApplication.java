97c0792fe1af6da224551e1ec83b79b2f81439bc
Tony Tang <tonytang213@gmail.com>
2023-05-03 12:47:34 -0500

    chore: initial commit

---
+++ src/main/java/api/v1/MetalloyEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package api.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalloyEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalloyEmailServiceApplication.class, args);
+    }
+
+}
