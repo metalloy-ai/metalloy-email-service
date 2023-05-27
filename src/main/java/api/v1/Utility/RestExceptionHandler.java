ddc8c1cbf34f5acbbf3cdfc6656e35cd5a044a70
tuan <tuan882612@gmail.com>
2023-05-27 12:56:19 -0500

    feat: added IOException to global exceptions - updated the other exception to a reactive model

--- src/main/java/api/v1/Utility/RestExceptionHandler.java
+++ src/main/java/api/v1/Utility/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package api.v1.Utility;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
@@ -8,14 +10,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+
 @Component
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleBadRequestException(MethodArgumentNotValidException e) {
-        APIResponse<Void> errorResponse = new APIResponse(400, e.getMessage(), null);
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<APIResponse<Void>> handleBadRequestException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new APIResponse<>(400, e.getMessage(), null));
     }
 
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<APIResponse<Void>> handleIOException(IOException e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new APIResponse<>(500, e.getMessage(), null));
+    }
 }
