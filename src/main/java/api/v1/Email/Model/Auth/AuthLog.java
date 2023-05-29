fb637d3eaac7fd5e983b55cc04d9ca32b40bf186
tuan <tuan882612@gmail.com>
2023-05-29 17:14:42 -0500

    feat: separate out the email to subgroups - Auth is now its own subgroup in email

--- src/main/java/api/v1/Email/Model/Auth/AuthLog.java
+++ src/main/java/api/v1/Email/Model/Auth/AuthLog.java
@@ -1,12 +1,12 @@
-package api.v1.Email;
+package api.v1.Email.Model.Auth;
 
+import api.v1.Email.Model.Auth.AuthCreate;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -17,21 +17,24 @@ import java.util.Random;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Document(collection = "auth")
-public class EmailModel extends EmailCreate {
+public class AuthLog extends AuthCreate {
     @Id
     @JsonProperty("_id")
     private String id;
     private String code;
-    @Indexed(name = "expire_1", expireAfterSeconds = 180)
-    private Date expire;
+    private String message;
+    private Date created;
 
-    public EmailModel(EmailCreate emailCreate) {
-        super(emailCreate.getUserId(), emailCreate.getUsername(), emailCreate.getEmail());
+    public AuthLog(AuthCreate authCreate) {
+        this.setUserId(authCreate.getUserId());
+        this.setUsername(authCreate.getUsername());
+        this.setRole(authCreate.getRole());
+        this.setEmail(authCreate.getEmail());
+        this.created = new Date();
         this.id = getUserId();
     }
 
-    public void generateCodeAndExpire() {
+    public void generateCode() {
         this.code = String.valueOf(new Random().nextInt(900000) + 100000);
-        this.expire = new Date();
     }
 }
