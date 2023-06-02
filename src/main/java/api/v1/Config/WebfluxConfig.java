9368c854945ec526189b9e38010bb16bbb8ef1b8
tuan <tuan882612@gmail.com>
2023-06-02 17:43:58 -0500

    feat: add user/auth origin to cors

--- src/main/java/api/v1/Config/WebfluxConfig.java
+++ src/main/java/api/v1/Config/WebfluxConfig.java
@@ -2,10 +2,19 @@ package api.v1.Config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 @EnableWebFlux
 @ComponentScan("org.springframework.web")
-public class WebfluxConfig implements WebFluxConfigurer { }
+public class WebfluxConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:2000")
+                .allowedMethods("PUT","POST","GET","DELETE","OPTIONS");
+
+    }
+}
